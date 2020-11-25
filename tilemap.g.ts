7c25fab97f505d878b37332783239397fac7a728
felixtsu <humaeks@gmail.com>
2020-11-25 14:24:57 +0800

    Initial files for MakeCode project

---
+++ tilemap.g.ts
@@ -0,0 +1,25 @@
+// Auto-generated code. Do not edit.
+namespace myTiles {
+    //% fixedInstance jres blockIdentity=images._tile
+    export const transparency16 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile1 = image.ofBuffer(hex``);
+
+    helpers.registerTilemapFactory(function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "level": return tiles.createTilemap(hex`0a00080001020202020202020203060b09090909090909080609090909090909090806090909090909090908060909090909090909080609090909090909090806090909090909090a0805070707070707070704`, img`
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+`, [myTiles.transparency16,sprites.dungeon.greenOuterNorthWest,sprites.dungeon.greenOuterNorth0,sprites.dungeon.greenOuterNorthEast,sprites.dungeon.greenOuterSouthWest,sprites.dungeon.greenOuterSouthEast,sprites.dungeon.greenOuterWest0,sprites.dungeon.greenOuterSouth1,sprites.dungeon.greenOuterEast0,sprites.dungeon.floorLight2,sprites.dungeon.doorOpenNorth,sprites.dungeon.stairNorth], TileScale.Sixteen)
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
