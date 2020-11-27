e67c71dcbda3e7862d1c11a9bd085a83ad2ff303
felixtsu <humaeks@gmail.com>
2020-11-27 20:39:19 +0800

    更新 hoc-maze.ts

--- hoc-maze.ts
+++ hoc-maze.ts
@@ -343,23 +343,23 @@ namespace maze {
         }
 
         summary.setUpSummaryScene(_challengerName, img`
-            . . . . f f f f . . . .
-            . . f f e e e e f f . .
-            . f f e e e e e e f f .
-            f f f f 4 e e e f f f f
-            f f f 4 4 4 e e f f f f
-            f f f 4 4 4 4 e e f f f
-            f 4 e 4 4 4 4 4 4 e 4 f
-            f 4 4 f f 4 4 f f 4 4 f
-            f e 4 d d d d d d 4 e f
-            . f e d d b b d d e f .
-            . f f e 4 4 4 4 e f f .
-            e 4 f b 1 1 1 1 b f 4 e
-            4 d f 1 1 1 1 1 1 f d 4
-            4 4 f 6 6 6 6 6 6 f 4 4
-            . . . f f f f f f . . .
-            . . . f f . . f f . . .
-        `)
+    . . . . . . . . . . . . . . . .
+    . . . . . . . . . . . . . . . .
+    . . . 1 1 1 1 1 1 1 1 1 1 . . .
+    . . 1 4 4 4 4 4 4 4 4 4 1 . . .
+    . 1 5 4 4 4 4 4 4 4 4 4 1 . . .
+    1 5 5 4 4 f f 4 4 1 1 1 1 1 1 1
+    5 5 5 4 4 f f 4 4 5 5 5 5 5 5 .
+    . . . 4 4 4 4 4 4 5 5 5 5 5 . .
+    . . . 4 4 4 4 4 4 5 5 5 5 . . .
+    . . . 4 4 4 4 4 4 5 5 5 1 . . .
+    . . . 4 4 4 4 4 4 5 5 4 1 . . .
+    . . . 4 4 4 4 4 4 5 4 4 1 . . .
+    . . . 9 9 9 7 7 7 7 7 7 1 . . .
+    . . . . 9 9 7 7 7 7 7 7 1 . . .
+    . . . . . 9 7 7 7 7 7 7 1 . . .
+    . . . . . . . . . . . . . . . .
+`)
         summary.textUp(_levelResults)
     })
     
